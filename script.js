7673d2ba1cfd6b26e73593e70b4bf463dccb1c1e
shawondas512 <sdshawondas512@gmail.com>
2026-07-04 02:17:15 +0600

    Update script.js  page

---
+++ script.js
@@ -0,0 +1,7 @@
+function unlockDiscount() {
+    alert("Surprise! You unlocked a 10% discount on your next booking. Use code: TRAVEL2026 at checkout.");
+}
+
+function sendInquiry() {
+    alert("Thank you! Your travel inquiry has been sent securely. Our team will contact you within 24 hours.");
+}
